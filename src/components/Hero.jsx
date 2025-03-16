d97bb6f9b0f8ec7e62afd26e3d2aa11a8521c712
muhananaufal <muhananaufal8@gmail.com>
2025-03-16 17:24:13 +0700

    fix hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 	const [loading, setLoading] = useState(true);
 	const [loadedVideos, setLoadedVideos] = useState(0);
 
-	const totalVideos = 4;
+	const totalVideos = 3;
 	const nextVdRef = useRef(null);
 
 	const handleVideoLoad = () => {
